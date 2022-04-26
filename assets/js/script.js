4cd9d2d5550c8c6c9f93ddc88c2e224707be535e
WebDev TVlean <tvleanjohnny@gmail.com>
2022-04-26 16:57:17 +0300

    Главная страница

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,24 @@ const reviewSlider = new Swiper('.reviewSlider', {
     slidesPerView: 2.3,
     spaceBetween: 30,
 });
+
+// --------- Fixed top menu 
+let topPanel = document.querySelector('.topPanel');
+let mainWrapper = document.querySelector('.mainWrapper');
+
+const fixedMenu = () => {
+    let height = topPanel.offsetHeight;
+    if(pageYOffset > topPanel.offsetHeight) {
+        topPanel.classList.add('active');
+        mainWrapper.setAttribute('style', 'padding-top: ' + height+ 'px;' ); 
+        topPanel.setAttribute('style', 'transform: translate(0px, 0px);' );  
+    } else if(pageYOffset < topPanel.offsetHeight) {
+        topPanel.classList.remove('active');
+        topPanel.removeAttribute('style', 'transform: translate(0px, 0px);' );
+        mainWrapper.removeAttribute('style', 'padding-top: ' + height + 'px;' ); 
+    }
+}
+fixedMenu();
+window.addEventListener('scroll', function() {
+    fixedMenu();
+});
